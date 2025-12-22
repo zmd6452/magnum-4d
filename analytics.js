140c04e831a40926006a9870e04a628146c08198
Zul <rukia6452@gmail.com>
2025-12-23 07:59:50 +0800

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,23 +1,24 @@
-function calculateFrequency(data) {
+function getFrequency(data) {
   const freq = {};
 
   data.forEach(draw => {
     draw.numbers.forEach(num => {
-      freq[num] = (freq[num] || 0) + 1;
+      for (let d of num) {
+        freq[d] = (freq[d] || 0) + 1;
+      }
     });
   });
 
-  return Object.entries(freq)
-    .sort((a,b) => b[1] - a[1])
-    .slice(0, CONFIG.maxFrequencyResults);
+  return freq;
 }
 
 function renderFrequency(freq) {
   const table = document.getElementById("freqTable");
-  table.innerHTML = "<tr><th>Number</th><th>Count</th></tr>";
+  table.innerHTML = "<tr><th>Digit</th><th>Count</th></tr>";
 
-  freq.forEach(([num, count]) => {
-    const row = table.insertRow();
-    row.innerHTML = `<td>${num}</td><td>${count}</td>`;
-  });
+  Object.entries(freq)
+    .sort((a,b)=>b[1]-a[1])
+    .forEach(([digit,count])=>{
+      table.innerHTML += `<tr><td>${digit}</td><td>${count}</td></tr>`;
+    });
 }
