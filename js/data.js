a6dfe693992e4c1d49036d3c4859b43f828e6749
Zul <rukia6452@gmail.com>
2025-12-22 23:33:48 +0800

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,10 @@
+// SAMPLE data (replace with real past results anytime)
+const drawData = [
+  { number: "1234" },
+  { number: "2419" },
+  { number: "9123" },
+  { number: "2108" },
+  { number: "7812" },
+  { number: "1290" },
+  { number: "4321" }
+];
